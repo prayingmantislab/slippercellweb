53783a0a26e76e3502353626f27e44f05d02eaae
Asaf Meir <office@elpuravida.co.il>
2021-02-06 22:50:14 +0200

    finshed google sign in

--- src/App.js
+++ src/App.js
@@ -1,63 +1,12 @@
-// import * as React from "react";
-// import {
-//   FirebaseAuthProvider,
-//   // FirebaseAuthConsumer
-// } from "@react-firebase/auth";
-// import firebase from "firebase/app";
-
-// import "./App.css";
-// import "firebase/auth";
-// import "firebase/firestore";
-
-// import { config } from "./test-credentials";
-
-// const IDontCareAboutFirebaseAuth = () => {
-//   return <div>this part won't react to firebase auth changes</div>
-// };
-// import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-// import Header from "./components/header/header.component";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD5H3PNsatfePZ5N0LRlb8v5yKQDCgt3yA",
-//   authDomain: "sleepercelll-web.firebaseapp.com",
-//   projectId: "sleepercelll-web",
-//   storageBucket: "sleepercelll-web.appspot.com",
-//   messagingSenderId: "477193796172",
-//     appId: "1:477193796172:web:08864695bfd9e32454d2e9",
-//     measurementId: "G-TGERGB0CN7"
-// }
-
-// firebase.initializeApp(firebaseConfig);
-
-
-// const App = () => {
-//   return (
-//     <div>
-//       <IDontCareAboutFirebaseAuth />
-//       <FirebaseAuthProvider {...config} firebase={firebase}></FirebaseAuthProvider>
-//     </div>
-//   )
-// }
-// export default App;
-
 import * as React from "react";
-import { render } from "react-dom";
 import {
   FirebaseAuthProvider,
-  FirebaseAuthConsumer
+  FirebaseAuthConsumer,
+  // IfUnAuthed
 } from "@react-firebase/auth";
 import firebase from "firebase/app";
 import "firebase/auth";
-
-const config = {
-  apiKey: "AIzaSyD5H3PNsatfePZ5N0LRlb8v5yKQDCgt3yA",
-  authDomain: "sleepercelll-web.firebaseapp.com",
-  projectId: "sleepercelll-web",
-  storageBucket: "sleepercelll-web.appspot.com",
-  messagingSenderId: "477193796172",
-  appId: "1:477193796172:web:08864695bfd9e32454d2e9",
-  measurementId: "G-TGERGB0CN7"
-}
+import { config } from "./test-credentials";
 
 const IDontCareAboutFirebaseAuth = () => {
   return <div>This part won't react to firebase auth changes</div>;
@@ -67,7 +16,7 @@ const App = () => {
   return (
     <div>
       <IDontCareAboutFirebaseAuth />
-      <FirebaseAuthProvider {...config} firebase={firebase}>
+      <FirebaseAuthProvider {...config} firebase={firebase} >
         <div>
           Hello <div>From Auth Provider Child 1</div>
           <FirebaseAuthConsumer>
@@ -78,10 +27,7 @@ const App = () => {
                     <h2>You're signed in 🎉 </h2>
                     <button
                       onClick={() => {
-                        firebase
-                          .app()
-                          .auth()
-                          .signOut();
+                        firebase.app().auth().signOut();
                       }}
                     >
                       Sign out
@@ -94,10 +40,7 @@ const App = () => {
                     <h2>You're not signed in </h2>
                     <button
                       onClick={() => {
-                        firebase
-                          .app()
-                          .auth()
-                          .signInAnonymously();
+                        firebase.app().auth().signInAnonymously();
                       }}
                     >
                       Sign in anonymously
@@ -108,7 +51,17 @@ const App = () => {
             }}
           </FirebaseAuthConsumer>
         </div>
-        <div>Another div</div>
+        <div>
+        <button
+        onClick={() => {
+          const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+          firebase.auth().signInWithPopup(googleAuthProvider);
+        }}
+      >
+        Sign in with Google
+      </button>
+
+        </div>
       </FirebaseAuthProvider>
     </div>
   );
--- src/test-credentials.js
+++ src/test-credentials.js
@@ -1,2 +1,11 @@
 
       
+export const config = {
+    apiKey: "AIzaSyD5H3PNsatfePZ5N0LRlb8v5yKQDCgt3yA",
+    authDomain: "sleepercelll-web.firebaseapp.com",
+    projectId: "sleepercelll-web",
+    storageBucket: "sleepercelll-web.appspot.com",
+    messagingSenderId: "477193796172",
+    appId: "1:477193796172:web:08864695bfd9e32454d2e9",
+    measurementId: "G-TGERGB0CN7"
+  }
